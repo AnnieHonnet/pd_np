df21ac12f886bdf0416363db478d3630c61fb872
AnnieHonnet <nguyenah1112@gmail.com>
2026-08-17 16:44:29 +0200

    17/08/2026

--- pand_numpy.py
+++ pand_numpy.py
@@ -1,3 +1,5 @@
+#!/Users/nfs/annie/.conda/envs/MISTRAL/bin/python
+
 import pandas as pd
 import numpy as np
 import os
@@ -5,6 +7,8 @@ import sys
 import time
 from pathlib import Path
 import re
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
 
 
@@ -13,6 +17,8 @@ import re
 
 DATA = Path("/home/anh/Desktop/pandas_bioinfo_training/data")
 
+K2_REPORT_PATH = Path("/Users/nfs/annie/projects/yousra/fastq_mouse/merge1")
+Metaphaln_table = Path("")
 REPORT_COLS = ["pct", "clade_reads", "direct_reads", "rank", "taxid", "name"]
 
 #EX1
@@ -74,13 +80,88 @@ def air():
     no2.to_csv("no2.csv")
     
     print(no2_subset)
-   
 
 
-# main
-if __name__ == "__main__": 
+
+def k2_report_tab():
+    COL_K2 = ["percent" , "clade_reads", "taxon_reads", "rank_code", "ncbi_id" , "scientific_name"]
+    dfs=[]
+    dfs_all=[]
+    all_samples=[]
+
+    for report in sorted(K2_REPORT_PATH.glob("*.report")): 
+        df = pd.read_csv(report, sep="\t", names=COL_K2, skipinitialspace=True )
+        df["sample"] = report.stem
+        all_samples.append(report.stem)
+        
+        Clostridium = df["scientific_name"].str.contains("Clostridioides difficile|Clostridium butyricum")
+        dfs.append(df[Clostridium])
+        
+        Species_specific = df["rank_code"].str.contains("S")
+        dfs_all.append(df[Species_specific])
+    k2_report_all = pd.concat(dfs_all, ignore_index=True)
+    #k2_report = pd.concat(dfs, ignore_index=True)
+    #print(k2_report)
+    #return k2_report
+    
+    table_all = k2_report_all.pivot_table(index="scientific_name", columns="sample", values="taxon_reads", fill_value=0)
+    #table = k2_report.pivot_table(index="scientific_name", columns="sample", values="taxon_reads", fill_value=0) 
+    
+    #table = table.reindex(columns=all_samples, fill_value=0) #force to have all samples inside table even if there are no C.diff or C.but inside
+
+
+    #print(table)
+    #table.plot()
+    #table.to_csv("k2_report_table.tsv",sep="\t")
+    table_all.to_csv("k2_report_table_all.tsv",sep="\t")
+ 
+metaphlan_tab= Path("/Users/nfs/annie/projects/yousra/fastq_mouse/metaphlan3/metaphlan_clostridium.csv")
+k2_tab = Path("/Users/nfs/annie/projects/yousra/fastq_mouse/metaphlan3/k2_repor_table.csv")
+plot_out = Path("/Users/nfs/annie/projects/yousra/fastq_mouse/metaphlan3/plots")
+species   = ["Clostridioides difficile", "Clostridium butyricum"]
+
+def load_table(path, sep="\t"):
+    df = pd.read_csv(path, sep=sep, index_col=0, thousands=",")
+    df.index = df.index.str.replace("_", " ").str.strip()
+    df.columns = [re.match(r"(DE-\d+-S\d+)", c).group(1) for c in df.columns]
+
+    return df 
+
+table_k2  = load_table(k2_tab, sep=",")
+table_mpa = load_table(metaphlan_tab, sep=",")
+
+samples = [s for s in table_k2.columns if s in table_mpa.columns]
+table_k2  = table_k2[samples]
+table_mpa = table_mpa[samples]
+
+plot_out.mkdir(parents=True, exist_ok=True)
+
+
+fig, axes = plt.subplots(2, 1, figsize=(22, 8), sharex=True)
+for ax, sp in zip(axes, species):
+    ax.plot(samples, table_k2.loc[sp],  marker="o", ms=3, label="Kraken2")
+    ax.plot(samples, table_mpa.loc[sp], marker="s", ms=3, label="MetaPhlAn4")
+    ax.set_yscale("symlog")
+    ax.set_ylabel("reads")
+    ax.set_title(sp, style="italic")
+    ax.legend()
+axes[-1].set_xticks(range(len(samples)))
+axes[-1].set_xticklabels(samples, rotation=90, fontsize=5)
+plt.tight_layout()
+plt.savefig(plot_out / "k2_vs_mpa_lines.png", dpi=200)
+plt.close()
+
+
+def main(): 
     #ex1()
     #ex2()
     #ex3()
     #table_metaphlan()
-    air()
+    #air()
+    k2_report_tab()
+    table_k2  = load_table(k2_tab)
+    table_mpa = load_table(metaphlan_tab)
+
+# main
+if __name__ == "__main__": 
+    main()
